caf6bf2454f1645d22a102a36aea040682ed47d3
sharif64 <mohammadsharifulislam64@gmail.com>
2021-07-22 08:51:18 +0600

    barr project

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+let navLinks = document.querySelector("#navLinks");
+function showMenu(){
+    navLinks.style.right = "0";
+}
+function closeMenu(){
+    navLinks.style.right = "-200px";
+}
